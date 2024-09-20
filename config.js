be457fdfb72a63673d9963b259a2c0aca71a3041
Shadyar Bakr <Shadyar.bakr@proton.me>
2024-09-21 01:51:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -53,72 +53,33 @@ const CONFIG = {
 	firstButtonsContainer: [
 		{
 			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
-		},
-		{
-			id: '2',
-			name: 'Mail',
-			icon: 'mail',
-			link: 'https://mail.protonmail.com/',
-		},
-		{
-			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
-		},
-		{
-			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
-		},
-		{
-			id: '5',
 			name: 'Reddit',
 			icon: 'glasses',
 			link: 'https://reddit.com',
 		},
 		{
-			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
+			id: '2',
+			name: 'X',
+			icon: 'twitter',
+			link: 'https://x.com/',
 		},
-	],
-
-	secondButtonsContainer: [
 		{
-			id: '1',
+			id: '3',
+			name: 'Youtube',
+			icon: 'youtube',
+			link: 'https://youtube.com/',
+		},
+		{
+			id: '4',
 			name: 'Music',
 			icon: 'headphones',
 			link: 'https://open.spotify.com',
 		},
 		{
-			id: '2',
-			name: 'twitter',
-			icon: 'twitter',
-			link: 'https://twitter.com/',
-		},
-		{
-			id: '3',
-			name: 'bot',
-			icon: 'bot',
-			link: 'https://discord.com/app',
-		},
-		{
-			id: '4',
-			name: 'Amazon',
-			icon: 'shopping-bag',
-			link: 'https://amazon.com/',
-		},
-		{
 			id: '5',
-			name: 'Hashnode',
-			icon: 'pen-tool',
-			link: 'https://hashnode.com/',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
 		},
 		{
 			id: '6',
@@ -146,12 +107,6 @@ const CONFIG = {
 					name: 'Dribbble',
 					link: 'https://www.dribbble.com',
 				},
-			],
-		},
-		{
-			icon: 'github',
-			id: '2',
-			links: [
 				{
 					name: 'Front',
 					link: 'https://www.reddit.com/r/Frontend/',
